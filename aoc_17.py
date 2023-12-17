82af593cd8050748ae8be74f8133a35c8b62bf9d
Nusan <truc@machin.com>
2023-12-17 19:38:39 +0100

    day 17: 2.5x faster by avoiding useless deep-copy

--- aoc_17.py
+++ aoc_17.py
@@ -62,65 +62,70 @@ visited={}
 #mindist,maxdist=0,3 # Part 1
 mindist,maxdist=3,10 # Part 2
 
-steps=0
-end=None
-while pool and not end:
-    p=heapq.heappop(pool)
-    for i,d in enumerate(dirs):
-        # direction should not do a 180
-        nobacktrack = (i+2)%4!=p.lastdir
-        # cannot keep the same direction for too long
-        maxlimit = i!=p.lastdir or p.lastdircount<maxdist
-        # must keep the same direction at least some number
-        minlimit = i==p.lastdir or p.lastdircount>mindist
-        if  p.lastdir<0 or (nobacktrack and maxlimit and minlimit):
-            # cannot go in a direction, if mindist would make it go outside the limit anyway
-            curmindist = (max(0,mindist-p.lastdircount) if i==p.lastdir else mindist) + 1
-            # compute final position in at least mindist blocks
-            posx,posy = p.posx+d[0]*curmindist, p.posy+d[1]*curmindist            
-            if posx<0 or posx>=gridx or posy<0 or posy>=gridy: continue
-            ndc=p.lastdircount
-            if i!=p.lastdir: ndc=0
-            # compute total cost of each steps
-            ncost=0
-            for j in range(1,curmindist+1):
-                ncost+=grid[p.posy+d[1]*j][p.posx+d[0]*j]
-                ndc+=1
-            vk=(posx,posy,i,ndc)
-            if vk in visited: continue
-            visited[vk]=True
-            np=copy.deepcopy(p)
-            #np.histo=copy.deepcopy(p.histo)
-            np.cost+=ncost
-            np.posx,np.posy=posx,posy
-            #np.histo.append((p.posx,p.posy))
-            np.lastdir=i
-            np.lastdircount=ndc
-            if posx==gridx-1 and posy==gridy-1: # goal reached
-                #np.histo.append((posx,posy))
-                end=np
-                break
-            #print(np)
-            heapq.heappush(pool, np)
-            #pool.append(np)
-    #pool.sort(key=lambda x: -x.cost)
-    #pool.sort(key=lambda x: -x.totalcost())
-    steps+=1
-    if steps%10000==0:
-        if len(pool)>0:
-            pp=pool[-1]
-            #print("=-----------=")
-            #pp.printmap()
-            print("Best: "+str(pp.totalcost())+" real:"+str(pp.cost)+" "+str(len(pool)))
-    #print(", ".join(str(p) for p in pool))
-    #break
+#@profile
+def launch():
+    steps=0
+    end=None
+    while pool and not end:
+        p=heapq.heappop(pool)
+        for i,d in enumerate(dirs):
+            # direction should not do a 180
+            nobacktrack = (i+2)%4!=p.lastdir
+            # cannot keep the same direction for too long
+            maxlimit = i!=p.lastdir or p.lastdircount<maxdist
+            # must keep the same direction at least some number
+            minlimit = i==p.lastdir or p.lastdircount>mindist
+            if  p.lastdir<0 or (nobacktrack and maxlimit and minlimit):
+                # cannot go in a direction, if mindist would make it go outside the limit anyway
+                curmindist = (max(0,mindist-p.lastdircount) if i==p.lastdir else mindist) + 1
+                # compute final position in at least mindist blocks
+                posx,posy = p.posx+d[0]*curmindist, p.posy+d[1]*curmindist            
+                if posx<0 or posx>=gridx or posy<0 or posy>=gridy: continue
+                ndc=p.lastdircount
+                if i!=p.lastdir: ndc=0
+                # compute total cost of each steps
+                ncost=0
+                for j in range(1,curmindist+1):
+                    ncost+=grid[p.posy+d[1]*j][p.posx+d[0]*j]
+                    ndc+=1
+                vk=(posx,posy,i,ndc)
+                if vk in visited: continue
+                visited[vk]=True
+                #np=copy.deepcopy(p)
+                #np=copy.copy(p)
+                np=Path()
+                #np.histo=copy.deepcopy(p.histo)
+                np.cost=p.cost+ncost
+                np.posx,np.posy=posx,posy
+                #np.histo.append((p.posx,p.posy))
+                np.lastdir=i
+                np.lastdircount=ndc
+                if posx==gridx-1 and posy==gridy-1: # goal reached
+                    #np.histo.append((posx,posy))
+                    end=np
+                    break
+                #print(np)
+                heapq.heappush(pool, np)
+                #pool.append(np)
+        #pool.sort(key=lambda x: -x.cost)
+        #pool.sort(key=lambda x: -x.totalcost())
+        steps+=1
+        if steps%50000==0:
+            if len(pool)>0:
+                pp=pool[-1]
+                #print("=-----------=")
+                #pp.printmap()
+                print("Best: "+str(pp.totalcost())+" real:"+str(pp.cost)+" "+str(len(pool)))
+        #print(", ".join(str(p) for p in pool))
+        #break
 
-print("===============")
-if end:
-    #print(end)
-    end.printmap()
-    print("Path found:"+str(end.cost))
-else:
-    print("No Path Found :(")
+    print("===============")
+    if end:
+        #print(end)
+        end.printmap()
+        print("Path found:"+str(end.cost))
+    else:
+        print("No Path Found :(")
 
+launch()
 print("       ",time.process_time()-t)
